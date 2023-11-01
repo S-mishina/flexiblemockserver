8826b532638451a311d6cbe681894a7d974afedb
S-mishina <seiryu.mishina@zozo.com>
2023-11-02 01:37:12 +0900

    add mock server
    Signed-off-by: S-mishina <seiryu.mishina@zozo.com>

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, make_response , Response
+import time
+app = Flask(__name__)
+
+@app.route('/<int:sleep_time>/<int:status_code>')
+def index(sleep_time, status_code):
+    if 100 <= status_code <= 599:
+        time.sleep(sleep_time)
+        return make_response(jsonify(sleep_time=sleep_time,status_code=status_code), status_code)
+    else:
+        return make_response(jsonify(err="Not status code"), 400)
+
+@app.route('/<int:sleep_time>/', defaults={'status_code': 200})
+@app.route('/<int:sleep_time>')
+def only_sleep_time(sleep_time, status_code):
+    time.sleep(sleep_time)
+    return make_response(jsonify(sleep_time=sleep_time, status_code=status_code), status_code)
+
+@app.route('/status/<int:status_code>')
+def only_status_code(status_code, sleep_time=0):
+    if 100 <= status_code <= 599:
+        time.sleep(sleep_time)
+        return make_response(jsonify(sleep_time=sleep_time, status_code=status_code), status_code)
+    else:
+        return make_response(jsonify(err="Not status code"), 400)
+
+if __name__ == '__main__':
+    app.run()
+
