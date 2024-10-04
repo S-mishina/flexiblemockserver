85f6d0df6c099c7c3c34f94785a91ab8fe721214
S-mishina <45090872+S-mishina@users.noreply.github.com>
2024-10-04 10:29:42 +0900

    Fix/load test config #44 (#48)
    * fix
    
    Signed-off-by: S-mishina <45090872+S-mishina@users.noreply.github.com>
    
    * fix
    
    Signed-off-by: S-mishina <45090872+S-mishina@users.noreply.github.com>
    
    * fix
    
    Signed-off-by: S-mishina <45090872+S-mishina@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: S-mishina <45090872+S-mishina@users.noreply.github.com>

--- sample_manifest/kubernetes/locust/sample/demo_test.py
+++ sample_manifest/kubernetes/locust/sample/demo_test.py
@@ -1,7 +1,8 @@
 from locust import HttpUser, task
-
+import time
 class User(HttpUser):
-    @task 
+    @task
     def get_employees(self) -> None:
         """Get a list of employees."""
         self.client.get("/")
+        time.sleep(1)  # 1秒待機
